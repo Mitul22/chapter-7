b168276952ff23c2dcc3363cff8eca0a747da635
Mitul Tandon <mitultandon2000@gmail.com>
2024-09-07 16:46:19 +1000

    .

--- example-3/variables.tf
+++ example-3/variables.tf
@@ -1,11 +1,13 @@
 # Sets global variables for this Terraform project.
 
 variable app_name {
+  default="tbim222"
 }
 
 variable location {
   default = "eastus"
 }
 
-variable kubernetes_version {    
+variable kubernetes_version {   
+  default="1.27.1" 
 }
